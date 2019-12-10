832469c04d7e23b64d35c237bb361b56cd3a1b91
arianza1210 <48575505+arianza1210@users.noreply.github.com>
2019-12-10 22:48:58 +0700

    Add files via upload

---
+++ team.h
@@ -0,0 +1,15 @@
+#ifndef team_h
+#define team_h
+#include <iostream>
+
+using namespace std;
+
+void team(){
+	cout<<"------------------------------------------"<<endl;
+	cout<<"1) Pandi Barep Arianza	 1817051002"<<endl;
+	cout<<"2) Yuan Ferdinand K.     1817051014"<<endl;
+	cout<<"3) Jerremy Hessa         1817051035"<<endl;
+	cout<<"4) Arbi Hidayatullah     1817051045"<<endl;	
+	cout<<"------------------------------------------"<<endl;
+}
+#endif
